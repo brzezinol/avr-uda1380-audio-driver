75241d8e4cc4e8540c23cf83f4101bdeb1b97607
brzezinol@gmail.com <brzezinol@gmail.com@01e92b6c-b0b9-9f08-6acb-718558752af7>
2014-07-22 14:33:03 +0000

    First project struct

---
+++ avr-uda1380-audio-driver/audio.h
@@ -0,0 +1,18 @@
+/*
+ * audio.h
+ *
+ * Created: 2014-07-22 16:14:06
+ *  Author: pbrzezinski
+ */ 
+
+
+#ifndef AUDIO_H_
+#define AUDIO_H_
+
+#define AUDIO_GAIN_LINEIN       0
+#define AUDIO_GAIN_MIC          1
+
+
+
+
+#endif /* AUDIO_H_ */
