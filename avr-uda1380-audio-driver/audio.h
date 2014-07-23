243a12ef2dd8bbf2b647373bc59918329004f581
brzezinol@gmail.com <brzezinol@gmail.com@01e92b6c-b0b9-9f08-6acb-718558752af7>
2014-07-23 06:53:50 +0000

    Pierwsza wersja

---
+++ avr-uda1380-audio-driver/audio.h
@@ -0,0 +1,22 @@
+/*
+ * audio.h
+ *
+ * Created: 2014-07-22 16:14:06
+ *  Author: pbrzezinski
+ */ 
+
+
+#ifndef AUDIO_H_
+#define AUDIO_H_
+
+
+#define HAVE_UDA1380
+#define HAVE_RECORDING
+#define AUDIO_GAIN_LINEIN       0
+#define AUDIO_GAIN_MIC          1
+
+#ifdef _AUDIOHW_H_
+#include "audiohw.h"
+#endif
+
+#endif /* AUDIO_H_ */
---
+++ avr-uda1380/avr-uda1380.c
@@ -0,0 +1,39 @@
+/*
+ * avr_uda1380.c
+ *
+ * Created: 2014-07-22 21:46:14
+ *  Author: pbrzezinski
+ */ 
+
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include "avr-i2c.h"
+#include "rbuff.h"
+#include "audio.h"
+
+uint8_t I2cRxRing[I2C_RX_RING_BUFFER_SIZE] = {0x55,0xAA,0x55};
+
+int main(void)
+{
+	DDRC |= 0b00110000; //port C  pins 4,5 as out
+	PORTC |= 0b00110000; // enable pullups on 4,5 pins
+	i2c_init_(0x45);                        //- F_TWI=100KHz
+	//ring_buff_init (&I2cRxBuff,I2cRxRing);  //- Slave-mode RX/TX
+	i2c_set_address_(0x5E);                 //- 8-bit address
+	i2c_enable_gc_();
+	
+	//i2c_write(0xF0, 1);
+	
+	//audiohw_init();
+	
+	//audiohw_postinit();
+	
+	sei();   
+	
+    while(1)
+    {
+        audiohw_init();
+		audiohw_postinit();
+		_delay_ms(1000);
+    }
+}
